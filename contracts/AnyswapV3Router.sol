1ecb673ab5bd4606f54e4484d3c9ca653125e2a8
jowenshaw <jowenshaw@gmail.com>
2021-06-07 14:49:17 +0800

    update solc version

--- contracts/AnyswapV3Router.sol
+++ contracts/AnyswapV3Router.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pragma solidity 0.8.1;
+pragma solidity >=0.8.2;
 
 interface ISushiswapV2Pair {
     event Approval(address indexed owner, address indexed spender, uint value);
--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.0;
+pragma solidity >=0.8.2;
 
 library SafeMath {
     function add(uint256 x, uint256 y) internal pure returns (uint256 z) {
